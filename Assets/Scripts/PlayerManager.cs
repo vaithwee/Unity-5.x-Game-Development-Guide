db5bdc2c6daad3d4abeef6c82dc35c9fd579498b
Vaith <vaithwee@yeah.net>
2017-06-07 15:50:47 +0800

    add animator

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+	private Animator animator;
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		animator = GetComponent<Animator>();
+	}
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		animator.SetFloat("Speed", h * h + v * v);
+		animator.SetFloat("Direction", h, 0.25f, Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.Space))
+		{
+			animator.SetBool("Jump", true);
+			Debug.Log("Jump");
+		}
+		else
+		{
+			animator.SetBool("Jump", false);
+		}
+	}
+}
---
+++ Assets/Scripts/ThirdPersonCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCam : MonoBehaviour {
+
+	public Transform follow;
+	public float distanceAway;
+	public float distanceUp;
+	public float smooth;
+	private Vector3 targetPosition;
+
+	/// <summary>
+	/// LateUpdate is called every frame, if the Behaviour is enabled.
+	/// It is called after all Update functions have been called.
+	/// </summary>
+	void LateUpdate()
+	{
+		targetPosition = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
+		transform.position = Vector3.Lerp(transform.position, targetPosition, smooth * Time.deltaTime);
+		transform.LookAt(follow);
+	}
+}
