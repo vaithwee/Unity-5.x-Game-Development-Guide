40c3a6f80432d4cd09f2b7ef8895b4eb64972774
Vaith <vaithwee@yeah.net>
2017-06-12 21:43:49 +0800

    add ui

---
+++ Assets/Scripts/C_9_8_1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_9_8_1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPressToggle(bool active)
+    {
+        Debug.Log("current toggle status is " + active);
+    }
+}
