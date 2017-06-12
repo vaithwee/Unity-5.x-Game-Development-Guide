9f58cd7ae898eeafc5fe75cf9fa33eae4062f970
Vaith <vaithwee@yeah.net>
2017-06-12 22:06:02 +0800

    add lesson 9

---
+++ Assets/Scripts/C_9_8_2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class C_9_8_2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private ToggleGroup toggleGroup;
+
+    private void Awake()
+    {
+        toggleGroup = GetComponent<ToggleGroup>();
+  
+    }
+
+    public void OnPressToggle()
+    {
+        Toggle select = null;
+        foreach(Toggle item in toggleGroup.ActiveToggles())
+        {
+            select = item;
+            break;
+        }
+        Debug.Log("choose is " + select.transform.FindChild("Label").GetComponent<Text>().text);
+    }
+}
---
+++ Assets/Scripts/C_9_9.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class C_9_9 : MonoBehaviour {
+
+    public Text volume;
+
+    public Slider slider;
+
+    float startVolumn = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+        volume.text = startVolumn.ToString();
+        slider.value = startVolumn;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPressSlider()
+    {
+        volume.text = slider.value.ToString("f2");
+    }
+}
